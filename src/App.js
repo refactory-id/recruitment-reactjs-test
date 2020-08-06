6479290704e20572bb53bf091e2d4bc3bae2551d
Samsul Arifin <arifinsam.sam@gmail.com>
2020-08-06 08:15:20 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import GitHubLogin from "./GithubLogin";
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {name && <h2>Hai {name}</h2>}
+        <GitHubLogin
+          clientId=""
+          redirectUri=""
+          onSuccess={(name) => setName(name)}
+          onFailure={(resp) => console.log(resp)}
+        />
       </header>
     </div>
   );
